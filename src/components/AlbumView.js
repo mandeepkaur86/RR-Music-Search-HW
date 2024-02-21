9d20ebd1c128659c98c425a72e8584bd8265db7b
mandeepkaur86 <mandeepkaurdia@gmail.com>
2024-02-21 07:18:53 +0530

    suspence

---
+++ src/components/AlbumView.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+
+function AlbumView() {
+  const { id } = useParams()
+  const [albumData, setAlbumData] = useState([])
+  const navigate = useNavigate()
+  useEffect(() => {
+    const API_URL = `http://localhost:4000/song/${id}`
+    const fetchData = async () => {
+      const response = await fetch(API_URL)
+      const resData = await response.json()
+      setAlbumData(resData.results)
+    }
+    fetchData()
+  }, [id])
+  const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+  const renderSongs = justSongs.map((song, i) => {
+    return (
+      <div key={i}>
+        <p>{song.trackName}</p>
+      </div>)
+  })
+  const navButton = () => {
+    return(
+        <div>
+            <button onClick={() => navigate(-1)}>Back</button>
+            <button onClick={() => navigate('/')}>home</button>
+        </div>
+    )
+}
+
+
+  return (
+    <div>
+      {renderSongs}
+      {navButton()}
+    </div>
+  )
+}
+
+export default AlbumView
